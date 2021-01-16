2570ea5c9724e675668b059dc503a373e5a2daa4
raimyy2m <raimymejia@gmail.com>
2021-01-15 20:34:07 -0400

    Rename to ButgetManagementApp

---
+++ BudgetManagementApp/Entities/Migrations/Configuration.cs
@@ -0,0 +1,21 @@
+﻿using System.Data.Entity.Migrations;
+
+namespace BudgetManagementApp.Entities.Migrations
+{
+    internal sealed class Configuration : DbMigrationsConfiguration<Entities.Models.ButgetManagementAppContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+            AutomaticMigrationDataLossAllowed = true;
+        }
+
+        protected override void Seed(Entities.Models.ButgetManagementAppContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
---
+++ BudgetManagementApp/Entities/Models/ProjectManagementAppContext.cs
@@ -0,0 +1,35 @@
+﻿using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using BudgetManagementApp.Entities.Migrations;
+
+namespace BudgetManagementApp.Entities.Models
+{
+    public class ButgetManagementAppContext : DbContext
+    {
+        public ButgetManagementAppContext() : base("name=ButgetManagementApp")
+        {
+            Database.SetInitializer(
+                new MigrateDatabaseToLatestVersion<ButgetManagementAppContext, Configuration>()
+            );
+        }
+
+        public DbSet<Subject> Subjects { get; set; }
+        public DbSet<Student> Students { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+
+            //modelBuilder.Entity<Payment>().Property(w => w.AmountPaid).HasPrecision(18, 2);
+        }
+
+        //public class PatientMappings : EntityTypeConfiguration<Patient>
+        //{
+        //    public PatientMappings()
+        //    {
+        //        HasRequired(c => c.PatientHealth).WithRequiredPrincipal(e => e.Patient);
+        //        HasRequired(c => c.PlateRegistration).WithRequiredPrincipal(e => e.Patient);
+        //    }
+        //}
+    }
+}
