5e42e76561a5a8104265b3f2b9f39c303a504103
raimyy2m <raimymejia@gmail.com>
2021-01-17 18:33:41 -0400

    Move ICategoryRepository to a separate class

--- BudgetManagementApp/Repositories/Repositories/Categories/CategoryRepository.cs
+++ BudgetManagementApp/Repositories/Repositories/Categories/CategoryRepository.cs
@@ -6,14 +6,6 @@ using BudgetManagementApp.Entities.Models;
 
 namespace BudgetManagementApp.Repositories.Repositories.Categories
 {
-    public interface ICategoryRepository
-    {
-        Task<IEnumerable<Category>> GetAll();
-        Task Create(Category category);
-        Task Update(Category category);
-        Task Delete(Category category);
-    }
-
     public class CategoryRepository : BaseRepository, ICategoryRepository
     {
         public CategoryRepository(BudgetManagementAppContext context) : base(context)
