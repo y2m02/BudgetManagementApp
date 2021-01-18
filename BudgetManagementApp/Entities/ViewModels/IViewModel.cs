7db5d1a9b550ce5e4c05578e7005591057a6ee5b
raimyy2m <raimymejia@gmail.com>
2021-01-17 20:58:59 -0400

    Add IViewModel.cs

---
+++ BudgetManagementApp/Entities/ViewModels/IViewModel.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+
+namespace BudgetManagementApp.Entities.ViewModels
+{
+    public interface IViewModel
+    {
+    }
+
+    public class Success<T> : IViewModel
+    {
+        public Success(T model)
+        {
+            Model = model;
+        }
+
+        public T Model { get; }
+    }
+
+    public class Error : IViewModel
+    {
+        public Error(string errorMessage)
+        {
+            ErrorMessage = "Hubo un error durante el proceso. " +
+                           $"Por favor, consulte a soporte: \n{errorMessage}";
+        }
+
+        public string ErrorMessage { get; }
+    }
+
+    public class Validation : IViewModel
+    {
+        public Validation(IEnumerable<string> validationErrors)
+        {
+            ValidationErrors = validationErrors;
+        }
+
+        public IEnumerable<string> ValidationErrors { get; }
+    }
+}
