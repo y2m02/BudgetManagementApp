5b0fb0e9236ff234db6c7789f3e30293717e6bb5
Raimy Mejia <raimym@goflowapp.com>
2021-01-26 13:14:44 -0400

    Add view model

--- BudgetManagementApp/Entities/Mappings/ProfileMapper.cs
+++ BudgetManagementApp/Entities/Mappings/ProfileMapper.cs
@@ -2,6 +2,7 @@
 using AutoMapper;
 using BudgetManagementApp.Entities.Models;
 using BudgetManagementApp.Entities.ViewModels.Categories;
+using BudgetManagementApp.Entities.ViewModels.SubTypes;
 using BudgetManagementApp.Entities.ViewModels.Types;
 
 namespace BudgetManagementApp.Entities.Mappings
@@ -25,6 +26,19 @@ namespace BudgetManagementApp.Entities.Mappings
                 .ForMember(destination => destination.InUse,
                     member => member.MapFrom(field => field.SubTypes.Any(x => !x.DeletedOn.HasValue)));
             CreateMap<TypeViewModel, Type>();
+
+            CreateMap<SubType, SubTypeViewModel>()
+                .ForMember(destination => destination.Id,
+                    member => member.MapFrom(field => field.TypeId))
+                .ForMember(destination => destination.TypeDescription,
+                    member => member.MapFrom(field => field.Type.Description))
+                .ForMember(destination => destination.CategoryId,
+                    member => member.MapFrom(field => field.Type.CategoryId))
+                .ForMember(destination => destination.CategoryDescription,
+                    member => member.MapFrom(field => field.Type.Category.Description))
+                .ForMember(destination => destination.InUse,
+                    member => member.MapFrom(field => field.AccountingMovements.Any(x => !x.DeletedOn.HasValue)));
+            CreateMap<SubTypeViewModel, SubType>();
             //    .ForMember(destination => destination.Used,
             //        member => member.MapFrom(field => field.Assignments.Count > 0));
 
