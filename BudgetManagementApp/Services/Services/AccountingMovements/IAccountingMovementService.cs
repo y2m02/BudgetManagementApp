3b8fc5b637a2ef818e002c9b52ae304dbc5dd9b3
Raimy Mejia <raimym@goflowapp.com>
2021-02-03 12:15:07 -0400

    Add service

---
+++ BudgetManagementApp/Services/Services/AccountingMovements/IAccountingMovementService.cs
@@ -0,0 +1,11 @@
+﻿using BudgetManagementApp.Entities.ViewModels.Base;
+using BudgetManagementApp.Services.Services.Base;
+
+namespace BudgetManagementApp.Services.Services.AccountingMovements
+{
+    public interface IAccountingMovementService : IBaseService
+    {
+        BaseReturnViewModel GetIncomes();
+        BaseReturnViewModel GetExpenses();
+    }
+}
