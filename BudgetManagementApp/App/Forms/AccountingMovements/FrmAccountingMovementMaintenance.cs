c576f43996fccfc5e2d47d27581eeae572ff0559
Raimy Mejia <raimym@goflowapp.com>
2021-02-03 18:10:10 -0400

    Add maintenance UI

---
+++ BudgetManagementApp/App/Forms/AccountingMovements/FrmAccountingMovementMaintenance.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BudgetManagementApp.Forms.AccountingMovements
+{
+    public partial class FrmAccountingMovementMaintenance : Form
+    {
+        public FrmAccountingMovementMaintenance()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmAccountingMovementMaintenance_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BtnSave_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BtnCancel_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void CbxCategory_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void CbxType_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
