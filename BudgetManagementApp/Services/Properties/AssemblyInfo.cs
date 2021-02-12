bd50f7d88c8a15335b2fa56dc6e8477b2f432748
Raimy Mejia <raimym@goflowapp.com>
2021-02-12 16:37:23 -0400

    Installation

--- BudgetManagementApp/Services/Properties/AssemblyInfo.cs
+++ BudgetManagementApp/Services/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Services")]
+[assembly: AssemblyTitle("BudgetManagementApp.Services")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Services")]
-[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyProduct("BudgetManagementApp")]
+[assembly: AssemblyCopyright("Copyright © Y2M14 2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
